3e263a1cc657d2c2e979093ecc65ccc6f6c283fa
Alzbeta Reinhardova <alzbetareinhardova@gmail.com>
2021-11-13 08:45:41 +0100

    progress

--- timetoshine.py
+++ timetoshine.py
@@ -1,12 +1,12 @@
 from _typeshed import Self
 from typing import List,AnyStr
 import math 
-
+from enum import Enum
 
 
 class Document:
     def Load_Document():
-        source = open('insurance.csv', 'r')
+        source = open('./data/insurance.csv', 'r')
         lines = source.readlines()
         source.close()
 
@@ -21,36 +21,37 @@ class Document:
         pass
 
 
-
-
+class Region(Enum):
+    Southeast = 0
+    Southwest = 1
+    Northeast = 2
+    Nortwest = 3
 
 
 class Row:
-    def __init__(self, column) -> None:
-        self.column = column 
+    def __init__(self) -> None:
         pass
 
-    def Parse_age():
+    def Parse_age(self) -> int:
         pass
 
-    def Parse_sex():
+    def Parse_sex(self) -> bool:
         pass
 
-    def Parse_bmi():
+    def Parse_bmi(self) -> float:
         pass
 
-    def Parse_children_count():
+    def Parse_children_count(self) -> int:
         pass
 
-    def Parse_smoker(self) -> int:
-        if x == 'yes':
-            x = 1 
-        else:
-            x = 0 
+    def Parse_smoker(self) -> bool:
         pass 
 
-    def Parse_region():
+    def Parse_region(self) -> Region:
         pass
 
-    def Parse_charges():
+    def Parse_charges(self):
         pass
+
+
+
